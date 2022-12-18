577a76b5ccde99c31a73614d0a228861fc0692e6
unihit <macbookpro@unihitui-MacBookPro.local>
2022-12-18 16:58:14 +0900

    프런트 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -18,6 +18,7 @@
 
 // 모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 // 라우팅
@@ -26,9 +27,12 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
-
 // use -> 미들웨어를 등록해주는 메서드
 app.use(express.static(`${__dirname}/src/public`));
+// bodyParser 미들웨어 등록
+app.use(bodyParser.json());
+// URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", home);
 
 module.exports = app;
--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,11 +11,23 @@ function login() {
     id: id.value,
     psword: psword.value,
   };
+
   fetch("/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(req),
-  });
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        location.href = "/";
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error(new Error("로그인 중 에러 발생"));
+    });
 }
